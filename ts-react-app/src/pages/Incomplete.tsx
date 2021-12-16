e6b5df4e0d1942ee3a20da275471c765bd5b6005
leehyeonju <billaa@naver.com>
2021-12-16 18:40:46 +0900

    feat : select event control

--- ts-react-app/src/pages/Incomplete.tsx
+++ ts-react-app/src/pages/Incomplete.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { DataGridPro } from "@mui/x-data-grid-pro";
 import { columns } from "../data/columns";
 import { rows } from "../data/rows";
@@ -6,21 +6,33 @@ import { rows } from "../data/rows";
 export default function Incomplete() {
   const columnData = useMemo(() => columns, [columns]);
   const rowData = useMemo(() => rows, [rows]);
+  const [selectionModel, setSelectionModel] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGridPro
-        rows={rowData}
-        columns={columnData}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-        isRowSelectable={(params: any) => params.row.OrderAmount > 1}
-        initialState={{
-          pinnedColumns: { left: ["orderDate"], right: ["age"] },
-        }}
-      />
-    </div>
+    <>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGridPro
+          rows={rowData}
+          columns={columnData}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+          isRowSelectable={(params: any) => params.row.OrderAmount > 1}
+          initialState={{
+            pinnedColumns: { left: ["orderDate"], right: ["age"] },
+          }}
+          onSelectionModelChange={(newSelectionModel: any) => {
+            setSelectionModel(newSelectionModel);
+            setIsSelected(!isSelected);
+            if (newSelectionModel.length > 0) {
+              setIsSelected(true);
+            }
+          }}
+        />
+      </div>
+      <h2>{isSelected ? "true" : "false"}</h2>
+    </>
   );
 }
