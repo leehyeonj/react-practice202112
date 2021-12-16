4896afd4c492c626c14696c30518ce5982caea77
leehyeonju <billaa@naver.com>
2021-12-16 17:58:13 +0900

    feat : data setting and selectable set

--- ts-react-app/src/pages/Incomplete.tsx
+++ ts-react-app/src/pages/Incomplete.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { DataGridPro } from "@mui/x-data-grid-pro";
 import { columns } from "../data/columns";
 import { rows } from "../data/rows";
 
 export default function Incomplete() {
+  const columnData = useMemo(() => columns, [columns]);
+  const rowData = useMemo(() => rows, [rows]);
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGridPro
-        rows={rows}
-        columns={columns}
+        rows={rowData}
+        columns={columnData}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
         disableSelectionOnClick
-        isRowSelectable={(params: any) => params.row.age > 40}
+        isRowSelectable={(params: any) => params.row.OrderAmount > 1}
         initialState={{
           pinnedColumns: { left: ["orderDate"], right: ["age"] },
         }}
