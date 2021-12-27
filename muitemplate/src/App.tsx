ffb1023b6fe977114d33dabe4c457dcbfb74ca87
leehyeonju <billaa@naver.com>
2021-12-27 10:49:19 +0900

    feat : tabs

--- muitemplate/src/App.tsx
+++ muitemplate/src/App.tsx
@@ -1,12 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import AgGrid from "./pages/AgGrid";
 
-function App() {
+function TabPanel(props: any) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      HELLO
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
+  );
+}
+function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    newValue: any
+  ) => {
+    setValue(newValue);
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, maxWidth: 480, bgcolor: "background.paper" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons
+          aria-label="visible arrows tabs example"
+          sx={{
+            [`& .${tabsClasses.scrollButtons}`]: {
+              "&.Mui-disabled": { opacity: 0.3 },
+            },
+          }}
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+          <Tab label="Item Four" />
+          <Tab label="Item Five" />
+          <Tab label="Item Six" />
+          <Tab label="Item Seven" />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <AgGrid />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </>
   );
 }
 
---
+++ muitemplate/src/pages/AgGrid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AgGrid = () => {
+  return <div>hi</div>;
+};
+
+export default AgGrid;
